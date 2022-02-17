783a4b48602202ac7bcf1b0920ae69017405c30f
elinb29 <elinbog@yandex.ru>
2022-02-17 17:45:14 +0300

    Последние правки

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    // theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
-    appBar: AppBar(
-      title: Text(
-        "Привет 4 курс",
-      ),
-    ),
-    body: Align(
-      alignment: Alignment(0, 0),
-      child: Text(
-        'Скорее бы вернуться в универ',
-        style: TextStyle(
-            fontSize: 42, fontWeight: FontWeight.bold, color: Colors.black),
-      ),
-    ),
-  )));
+          appBar: AppBar(
+            title: const Text(
+              "Привет 4 курс",
+              style: TextStyle(fontSize: 32),
+            ),
+          ),
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/images/fox.jpg'),
+                width: 200,
+              ),
+              Text(
+                'Привет',
+                style: TextStyle(fontSize: 30, fontFamily: 'Roboto', color: Colors.red),
+              )
+            ],
+          ))));
 }
