9240aec3164671b3419869a050263dca3d82b2fd
elinb29 <elinbog@yandex.ru>
2022-02-22 15:52:19 +0300

    Классы

---
+++ lib/Fox.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Fox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage('assets/images/fox.jpg'),
+      height: 200,
+    );
+  }
+}
